5118c219e2a2a59776b5a169df17c4797080f594
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-25 19:46:54 +0200

    add function for taking user input

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 //play a five round game of rock paper scissors
 
 //take the users input(case insensitive)
+function takeUserInput(){
 
+    let userInput = prompt("Do you want to play rock, paper or scissors?").toString().toLowerCase();
+    let notVerifiedInput = true;
+    console.log(userInput)
+
+    while (notVerifiedInput){
+
+        if(userInput == "rock" || userInput == "paper" || userInput == "scissors"){
+            notVerifiedInput = false;
+        }else{
+            userInput = prompt("Please use only 'rock', 'paper' or 'scissors'!").toString().toLowerCase();
+            console.log(userInput);
+        }
+    }
+    
+    return userInput;
+}
+
+let test = takeUserInput();
+console.log(test)
 //generate the computers input (random)
 
 //play a single round 
