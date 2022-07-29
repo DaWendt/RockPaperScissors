97182d65b82cbcfc86fe92ace4566f1408fc0569
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-29 17:30:17 +0200

    correct width of gamefeed and add structural comments to js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 //play a five round game of rock paper scissors
 //game();
 
+
+//set up eventlisteners for all buttons
+//let the game run until either the player or the computer has 5 wins
+//as soon as a button is fired, let the computer generate a corresponding move
+//evaluate a winner
+//update the score 
+//write a message to the gamefeed
+//should the reset button be pressed, reset the game
