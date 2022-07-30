5acbe4ae771c934c56a75bd0947d1d44ed02df7b
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-30 09:13:02 +0200

    correct placement of script link and add eventListener to buttons

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //play a five round game of rock paper scissors
-//game();
-
 
 //set up eventlisteners for all buttons
+let buttons = document.querySelectorAll("button.PlayButton");
+console.log(buttons);
+buttons.forEach((button) => {
+
+    button.addEventListener("click", (e) => {
+        console.log(e);
+    });
+
+});
+
 //let the game run until either the player or the computer has 5 wins
 //as soon as a button is fired, let the computer generate a corresponding move
 //evaluate a winner
