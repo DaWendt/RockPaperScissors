e1e0a91e1c88b49307b20f75f6a7f37c2c0c05f2
Daniel Wendt <daniel.wendt@wendt-basis.de>
2022-07-30 12:54:15 +0200

    add function for computer's input

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-//play a five round game of rock paper scissors
-
 //set up eventlisteners for all buttons
 let buttons = document.querySelectorAll("button.PlayButton");
 console.log(buttons);
 buttons.forEach((button) => {
 
     button.addEventListener("click", (e) => {
-       console.log(e.currentTarget)
+       let clickedButton = e.currentTarget;
+       switch (clickedButton.id) {
+        case "Rock":
+            playRound("Rock")
+            break;
+        case "Paper":
+            playRound("Paper")
+            break;
+        case "Scissors":
+            playRound("Scissors")
+            break;
+       }
     });
-
 });
 
-//let the game run until either the player or the computer has 5 wins
 //as soon as a button is fired, let the computer generate a corresponding move
+function playRound(userInput){
+
+}
+
+function generateComputerInput(){
+
+    let number = Math.floor((Math.random() * 3) + 1);
+    switch (number) {
+        case 1:
+            return "Rock"
+            break;
+        case 2:
+            return "Paper"
+            break;
+        case 3:
+            return "Scissors"
+            break;
+    }
+}
+
 
 //evaluate a winner
 //update the score 
